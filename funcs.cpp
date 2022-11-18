ef05a14552988b5ca41074dbf64b27f9bd7c7e05
Shenghao <shenghaodong18@gmail.com>
2022-11-18 11:11:15 -0500

    started working on part c

--- funcs.cpp
+++ funcs.cpp
@@ -1,12 +1,29 @@
 #include <iostream>
 #include "classes.h"
 
+//Functions for testing
 void printTime(Time time){
     std::cout << time.h << ":" << time.m;
 }
 
 int getMinutes(Time time){
     return (time.h * 60) + time.m;
+}
+
+Time getTime(int minutes){
+    
+}
+
+void printMovie(Movie mv){
+    string g;
+    switch (mv.genre) {
+        case ACTION   : g = "ACTION"; break;
+        case COMEDY   : g = "COMEDY"; break;
+        case DRAMA    : g = "DRAMA";  break;
+        case ROMANCE  : g = "ROMANCE"; break;
+        case THRILLER : g = "THRILLER"; break;
+    }
+    cout << mv.title << " " << g << " (" << mv.duration << " min)";
 }
 
 //Part A
@@ -32,5 +49,33 @@ Time addMinutes(Time time0, int min){
     returnTime.m = totalMin%60;
     return returnTime;
 }
-    //addMinutes({8, 10}, 75)
-    // ==> should return {9, 25}
+
+//Part C
+//the printTimeSlot function should be renamed getTimeSlot and it should return a std::string. Run from main and test via doctests.
+std::string getTimeSlot(Timeslot ts){
+    std::string returnTimeSlot;
+    //Back to the Future COMEDY (116 min) [starts at 9:15, ends by 11:11] <-- Format
+    Movie movie = ts.Movie;
+    Time time = ts.Time;
+    //Back to the Future COMEDY (116 min) [starts at 9:15, ends by 
+    std::string += movie.title << " " << movie.genre << " (" << movie.duration << ")" + "[starts at " << printTime(time) << ", ends by ";
+
+}
+// class Time { 
+// public:
+//     int h;
+//     int m;
+// };
+
+// class Movie { 
+// public: 
+//     std::string title;
+//     Genre genre;     // only one genre per movie
+//     int duration;    // in minutes
+// };
+
+// class TimeSlot { 
+// public: 
+//     Movie movie;     // what movie
+//     Time startTime;  // when it starts
+// };
--- funcs.h
+++ funcs.h
@@ -1,7 +1,11 @@
 #pragma once
 #include "classes.h"
+
+//For Testing and other stuff
 void printTime(Time time);
 int getMinutes(Time time);
+void printMovie(Movie mv);
+Time getTime(int minutes);
 
 //Part A
 int minutesSinceMidnight(Time time);
@@ -9,3 +13,6 @@ int minutesUntil(Time earlier, Time later);
 
 //Part B
 Time addMinutes(Time time0, int min);
+
+//Part C
+std::string getTimeSlot(Timeslot ts);
